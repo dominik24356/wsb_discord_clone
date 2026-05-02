103b9673519828da1749f0bf883a35286f8ad53e
Dominik Szczepanik <dominik16333@gmail.com>
2026-05-02 18:38:53 +0200

    sidebar inclusion tag, profile edit redesign, sidebar unified across all views

---
+++ chat/templatetags/chat_tags.py
@@ -0,0 +1,16 @@
+from django import template
+from chat.models import Channel
+
+register = template.Library()
+
+@register.inclusion_tag('chat/sidebar.html', takes_context=True)
+def sidebar(context):
+    request = context['request']
+    text_channels = Channel.objects.filter(channel_type='text', is_public=True)
+    voice_channels = Channel.objects.filter(channel_type='voice', is_public=True)
+    return {
+        'request': request,
+        'text_channels': text_channels,
+        'voice_channels': voice_channels,
+        'current_channel': context.get('channel', None),
+    }
